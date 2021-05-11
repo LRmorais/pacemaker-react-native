982982e335b29679d756d339ca0b5179b20e5ca5
Lucas Morais <lucas.morais23ti@gmail.com>
2021-05-11 17:10:04 -0300

    Tela de login estruturada

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,23 +1,47 @@
+/* eslint-disable global-require */
 import React from 'react';
 import {
-  Text, View, SafeAreaView, Image, TouchableOpacity, ScrollView,
+  View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, Platform,
 } from 'react-native';
+import logo from '../../assets/logo.png';
+import Estilos from './styles';
 
 // import { Container } from './styles';
 
-const LoginScreen = ({ navigation }) => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login Screen</Text>
-      <TouchableOpacity style={{ marginTop: 20 }} onPress={() => navigation.navigate('HomeApp')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+const LoginScreen = ({ navigation }) => {
+  const ok = 20;
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={Estilos.background}
+    >
+      <View style={Estilos.containerLogo}>
+        <Image source={logo} />
+      </View>
 
-      <TouchableOpacity style={{ marginTop: 20 }} onPress={() => navigation.navigate('Register')}>
-        <Text>Register</Text>
-      </TouchableOpacity>
-    </View>
-  </SafeAreaView>
-);
+      <View style={Estilos.container}>
+        <TextInput
+          style={Estilos.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TextInput
+          style={Estilos.input}
+          placeholder="Password"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TouchableOpacity style={Estilos.btnSubmit} onPress={() => navigation.navigate('HomeApp')}>
+          <Text style={Estilos.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={Estilos.btnRegister} onPress={() => navigation.navigate('Register')}>
+          <Text style={Estilos.registerText}>Criar conta</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default LoginScreen;
