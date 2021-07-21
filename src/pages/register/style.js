026757a500b20fca35ed5bcd289b65709b7981f4
Lucas Morais <lucas.morais23ti@gmail.com>
2021-07-20 23:53:44 -0300

    change style register screen

---
+++ src/pages/register/style.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0B0633',
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    marginBottom: '30%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    padding: 15,
+    borderRadius: 10,
+  },
+  btnSubmit: {
+    backgroundColor: '#FF4949',
+    width: '90%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
+  submitText: {
+    color: '#fff',
+    fontSize: 17,
+  },
+  btnRegister: {
+    marginTop: 15,
+  },
+  registerText: {
+    color: '#fff',
+  },
+
+});
