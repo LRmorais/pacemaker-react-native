7300aaaeb4355b87e7bee67c8dda4f9aa76fa1ef
Lucas Morais <lucas.morais23ti@gmail.com>
2021-09-19 17:27:28 -0300

    feat: context global

--- src/hocs/globalState.js
+++ src/hocs/globalState.js
@@ -8,8 +8,16 @@ export const useGlobalStateContext = () => useContext(Context);
 const GlobalStateProvider = (props) => {
   const [generalData, setGeneralData] = useState(null);
 
+  const userInfo = {
+    name: 'Maria da Silva e Silva',
+    documentNumber: '0015248-2',
+    birthday: '20/05/1995',
+    cpf: '264.587.892-10',
+  };
+
   const value = {
     generalData,
+    userInfo,
   };
 
   return <Context.Provider value={value}>{props.children}</Context.Provider>;
