3117db95e12a42a19395353b84a175f10a3a77b0
Lucas Morais <lucas.morais23ti@gmail.com>
2021-09-19 17:26:28 -0300

    feat: change routes and providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import HOCs from './hocs';
 import Routes from './App';
 
 const NewApp = () => (
-  <HOCs>
-    <Routes />
-  </HOCs>
+  <PaperProvider>
+    <HOCs>
+      <Routes />
+    </HOCs>
+  </PaperProvider>
 );
 
 export default NewApp;
